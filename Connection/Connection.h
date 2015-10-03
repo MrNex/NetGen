8916978da7ba3a13e836ba6a193305e672081fbc
Clifton Somers <csomers@purdue.edu>
2015-10-03 17:03:38 -0400

    done

--- Connection/Connection.h
+++ Connection/Connection.h
@@ -50,4 +50,23 @@ void Connection_Open(Connection* conn);
 //	conn: Pointer to The connection to terminate
 void Connection_Close(Connection* conn);
 
-#endif
+
+
+
+typedef struct tcpConnection 
+{
+	int socketFD;// File descriptor that represents this connection stream
+	struct sockaddr_in clientAddr;// the connection holder specifics
+		/*struct sockaddr_in {
+			    short            sin_family;   // e.g. AF_INET
+			    unsigned short   sin_port;     // e.g. htons(3490)
+			    struct in_addr   sin_addr;     // see struct in_addr, below
+			    char             sin_zero[8];  // zero this if you want to
+		};
+
+		struct in_addr {
+			    unsigned long s_addr;  // load with inet_aton()
+		};*/
+
+} Connection;
+
